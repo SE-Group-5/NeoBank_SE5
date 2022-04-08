5c38c98b35f4f6b2bca3d2f16a7f596b77da2f80
Gowtham Gorrepati <gowtham.gorrepati30@gmail.com>
2022-04-08 21:26:59 +0530

    Add basic crypto page

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Home from "./components/Home";
+import Crypto from "./components/crypto/Crypto";
 
 const SiteRouter = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" exact element={<Home />} />
+				<Route path="/crypto" element={<Crypto />} />
 			</Routes>
 		</BrowserRouter>
 	);
