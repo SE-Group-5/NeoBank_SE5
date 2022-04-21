b9fec51eda0dec59c659e35509e105cf67f14e04
Shiva Reddy <59078131+shiva-reddy-1001@users.noreply.github.com>
2022-04-21 15:19:25 +0530

    ~
    ~

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import Card from './CardManagement/Card';
+import Reward from './CardManagement/Reward';
 
 function App() {
   return (
-    <></>
+    <>
+    <Card cardCvv={1245} cardBrand={"VISA"} cardNumber={"4 4 0 2 1 2 3 4 5 5 4 2 1 3 2 1"} 
+      cardExpiry={"12/2021"}/> 
+    <Reward rewardTitle={"Reward"} rewardPoints={512} ></Reward>
+    </>
   );
 }
 
